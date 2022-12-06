078e91aab62e1714b5128ad4c7d95b0f14973e09
ArjunAruz <a-10375@ibsplc.com>
2022-12-06 10:46:20 +0530

    [TECH-531] Adding unit test cases and interceptor part

---
+++ src/test/java/com/hotel/mappers/request/raterule/RuleMessageMapperTest.java
@@ -0,0 +1,31 @@
+package com.hotel.mappers.request.raterule;
+
+import com.hotel.service.raterule.HotelRateRuleRequest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.opentravel.ota._2003._05.OTAHotelBookingRuleRQ;
+
+import java.math.BigInteger;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+@ExtendWith(MockitoExtension.class)
+public class RuleMessageMapperTest {
+
+    @InjectMocks
+    RuleMessageMapper ruleMessageMapper;
+
+    @Test
+    public void testMapRuleMessage() {
+        HotelRateRuleRequest request = HotelRateRuleRequest.newBuilder().
+                setHotelCode("abc").
+                setRatePlanId("1234").build();
+
+        OTAHotelBookingRuleRQ.RuleMessage ruleMessage = ruleMessageMapper.mapRuleMessage(request);
+        assertThat(ruleMessage).isNotNull();
+        assertThat(ruleMessage.getHotelCode()).isEqualTo("abc");
+        assertThat(ruleMessage.getStatusApplication().getRatePlanID()).isEqualTo("1234");
+    }
+}
