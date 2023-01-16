93bb1c0c7d08587b65d900a1f2365ce5e2e2186a
Rithin Remash <a-10815@ibsplc.com>
2023-01-16 10:35:02 +0530

    Error Externalization

--- src/main/java/com/hotel/HotelBookRepriceApplication.java
+++ src/main/java/com/hotel/HotelBookRepriceApplication.java
@@ -1,12 +1,29 @@
 package com.hotel;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+import com.hotel.util.ErrorMappings;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.IOException;
 
 @SpringBootApplication
 @EnableEurekaClient
 public class HotelBookRepriceApplication {
     public static void main(String[] args) {
         SpringApplication.run(HotelBookRepriceApplication.class, args);}
+
+    @Bean
+    public ErrorMappings errorMap() throws IOException {
+        ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
+        mapper.findAndRegisterModules();
+        return mapper.readValue(new ClassPathResource("/error.yaml")
+                        .getInputStream(),
+                ErrorMappings.class);
+
+    }
 }
